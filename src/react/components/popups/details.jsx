f382c8b0ec176c35d8259115c871e1aa5769992c
Rikky <rikkster@yandex.ru>
2021-09-20 17:17:50 +0300

    Модальное окно 'Детальнее о'

---
+++ src/react/components/popups/details.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Popup } from './popup.controller';
+
+export const Details = ( props ) => {
+
+  return (
+
+    <Popup>
+      <p>123</p>
+    </Popup>
+
+  );
+
+}
