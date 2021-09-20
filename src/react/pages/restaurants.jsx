a4a9cc28c1cf8dcd5d0872ccd166b09133327380
Rikky <rikkster@yandex.ru>
2021-09-20 18:13:55 +0300

    Страница ресторанов

---
+++ src/react/pages/restaurants.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContentHead from '../components/content.head';
+
+const Restaurants = () => (
+
+  <React.Fragment>
+
+    <ContentHead />
+    
+  </React.Fragment>
+  
+);
+
+export default Restaurants;
