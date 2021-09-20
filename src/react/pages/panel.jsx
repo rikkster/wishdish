c485bec3725946703e7128ffd5716d8a4ba88b47
Rikky <rikkster@yandex.ru>
2021-09-20 11:23:16 +0300

    Панель администратора: навигация и роутинг контента

---
+++ src/react/pages/panel.jsx
@@ -0,0 +1,48 @@
+import React, { lazy, Suspense } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Loader from '../components/loader';
+import Header from '../components/header';
+import SidebarMenu from '../components/menu';
+
+const Dishes = lazy(() => import('../pages/dishes'));
+const Restaurants = lazy(() => import('../pages/restaurants'));
+const Checkins = lazy(() => import('../pages/checkins'));
+
+const Panel = () => {
+
+  return (
+
+    <React.Fragment>
+
+      <Header />
+
+      <section className="content">
+
+        <SidebarMenu />
+
+        <section className="workflow">
+
+          <Suspense fallback={ <Loader /> }>
+
+            <Switch>
+
+              <Route exact path='/dishes' component = { Dishes } />
+              <Route exact path='/restaurants' component = { Restaurants } />                
+              <Route exact path='/checkins' component = { Checkins } />                
+
+            </Switch>
+
+          </Suspense>
+
+        </section>
+
+      </section>
+
+    </React.Fragment>
+
+  );
+
+}
+
+export default Panel;
