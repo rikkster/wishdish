fed623bc03603267161d0a89793f5b8bfe8d898e
Rikky <rikkster@yandex.ru>
2021-09-20 18:13:14 +0300

    Страница чекинов

---
+++ src/react/pages/checkins.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ContentHead from '../components/content.head';
+
+const Checkins = () => {
+  
+  return (
+
+    <React.Fragment>
+
+      <ContentHead />
+      
+    </React.Fragment>
+    
+  );
+
+}
+
+export default Checkins;
