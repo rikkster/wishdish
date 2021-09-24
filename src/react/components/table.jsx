96afafad5b77beedad1c8acd7837b0f9c5f6f897
Rikky <rikkster@yandex.ru>
2021-09-24 14:04:38 +0300

    Настройки вывода в виде таблицы для разных видов страниц

--- src/react/components/table.jsx
+++ src/react/components/table.jsx
@@ -1,48 +1,19 @@
 import React from 'react';
+import useGlobal from '../../store';
+import { getVariant } from '../../helpers';
+import Rating from './rating';
 
 const Table = ({ rows }) => {
 
-  const TABLE_HEAD = {
+  const [ globalState, globalActions ] = useGlobal();
 
-    dishes: [
-      {
-        name: "IMG",
-        sort: false
-      },
-      {
-        name: "ID",
-        sort: true
-      },
-      {
-        name: "Name",
-        sort: true
-      },
-      {
-        name: "Check-ins",
-        sort: true
-      },
-      {
-        name: "Date",
-        sort: true
-      },
-      {
-        name: "Count of tags",
-        sort: true
-      },
-      {
-        name: "Tags",
-        sort: false
-      }
-    ]
-  }
-
-  console.log(`table rows ${typeof rows}`, rows);
+  const variant = getVariant();
 
   return (
 
-    <div className = "table-container">
+    <div className = {`table-container ${ variant }`}>
 
-      <Row head content = { TABLE_HEAD.dishes } />
+      <Row head content = { TABLE_HEAD[ variant ] } />
 
       <div className="rows">
 
@@ -52,6 +23,12 @@ const Table = ({ rows }) => {
 
             key = { key }
             content = { content }
+            maxCols = { TABLE_COLUMNS[ variant ] }
+            action = { ( id ) => {
+              globalActions.changeState( "details", content ); //TEST
+              globalActions.changeState( "detailsSelectedPhoto", content.media[0] || "" ); //TEST
+              globalActions.popup.showPopup( "details", id )
+            }}
 
           />
 
@@ -65,46 +42,53 @@ const Table = ({ rows }) => {
 
 }
 
-const Row = ({ content = [], head = false }) => {
+const Row = ({ content = [], head = false, action = false, maxCols = false }) => {
 
   return (
   
-    <div className = {`row ${ head ? "row-head" : "" }`}>
+    <div 
+      className = {`row ${ head ? "row-head" : "" }`}
+      onClick = { () => ( action && content.id ) && action( content.id ) }
+    >
 
       { content && Object.keys( content ).map(( col, colName ) => (
+
+        ( !maxCols || colName < maxCols ) &&
       
-        <div key={ colName }>
-          
-          { col !== "img" 
-          
-            ? !head 
-
-              ? content[ col ] 
-
-              : <React.Fragment> 
-
-                  <text>{ content[ col ].name }</text>
-                  
-                  { content[ col ].sort && 
-                    
-                    <div 
-                      onClick={() => alert(`sort by ${col}`)} 
-                      class="icon" 
-                    /> 
-                    
-                  }
+          <div key = { colName }>
             
-                </React.Fragment>
+            { col !== "img" 
             
-            : <div 
-                className="photo" 
-                style={{ backgroundImage: `url(${ content[ col ] })` }}
-              />
+              ? !head 
+
+                ? col !== "rating" 
+                
+                  ? content[ col ]
+                  : <Rating value = { content[ col ] } />
+
+                : <React.Fragment> 
+
+                    <text>{ content[ col ].name }</text>
+                    
+                    { content[ col ].sort && 
+                      
+                      <div 
+                        onClick={() => alert(`sort by ${col}`)} 
+                        className = "icon" 
+                      /> 
+                      
+                    }
               
-          }
-          
-        </div>
-
+                  </React.Fragment>
+              
+              : <div 
+                  className="photo" 
+                  style={{ backgroundImage: `url(${ content[ col ] })` }}
+                />
+  
+            }
+            
+          </div>
 
       ))}
 
@@ -115,3 +99,128 @@ const Row = ({ content = [], head = false }) => {
 }
 
 export default Table;
+
+const TABLE_COLUMNS = {
+
+  dishes: 7,
+  restaurants: 5,
+  checkins: 7,
+  users: 5
+
+}
+    
+const TABLE_HEAD = {
+
+  dishes: [
+    {
+      name: "IMG",
+      sort: false
+    },
+    {
+      name: "ID",
+      sort: true
+    },
+    {
+      name: "Name",
+      sort: true
+    },
+    {
+      name: "Check-ins",
+      sort: true
+    },
+    {
+      name: "Date",
+      sort: true
+    },
+    {
+      name: "Count of tags",
+      sort: true
+    },
+    {
+      name: "Tags",
+      sort: false
+    }
+
+  ],
+
+  restaurants: [
+    {
+      name: "IMG",
+      sort: false
+    },
+    {
+      name: "ID",
+      sort: true
+    },
+    {
+      name: "Name",
+      sort: true
+    },
+    {
+      name: "Check-ins",
+      sort: true
+    },
+    {
+      name: "Address",
+      sort: false
+    },
+  
+  ],
+
+  checkins: [
+    {
+      name: "IMG",
+      sort: false
+    },
+    {
+      name: "ID",
+      sort: true
+    },
+    {
+      name: "User name",
+      sort: true
+    },
+    {
+      name: "Restaurant name",
+      sort: true
+    },
+    {
+      name: "Dish name",
+      sort: true
+    },
+    {
+      name: "Rating",
+      sort: true
+    },
+    {
+      name: "Date & time",
+      sort: true
+    }
+
+  ],
+
+  users: [
+    {
+      name: "IMG",
+      sort: false
+    },
+    {
+      name: "ID",
+      sort: true
+    },
+    {
+      name: "Name",
+      sort: true
+    },
+    {
+      name: "Check-ins",
+      sort: true
+    },
+    {
+      name: "Sign date",
+      sort: true
+    },
+
+  ]
+
+}
